74b3629b6b5cf66cb18ebe1c330677a9666e0d96
TisaGore <fffs22aa@mail.ru>
2024-05-20 19:04:16 +0500

    Testing

--- drawer.py
+++ drawer.py
@@ -3,17 +3,17 @@ from entities_and_objects import Block, Entity
 # from level_generator import LevelGenerator
 from constants import BLOCK_SIZE, COLORS
 
-class EntityDrawer:
+class EntityView:
     @staticmethod
     def draw(color: tuple[int, int, int], display: pg.Surface, entity: Entity):
         pg.draw.rect(display, COLORS[color], (*entity.hitbox.get_position(), *entity.hitbox.size), 6, 1)
 
-class ObjectDrawer:
+class ObjectView:
     @staticmethod
     def draw(color: tuple[int, int, int], display: pg.Surface, object: Block):
         pg.draw.rect(display, COLORS[color], (*object.hitbox.get_position(), *object.hitbox.size))
 
-class LevelDrawer:
+class LevelView:
     @staticmethod
     def draw_level(display: pg.Surface, level):
         column_len = len(level[0])
@@ -23,4 +23,4 @@ class LevelDrawer:
                     # print(1)
                     block = Block(size=(BLOCK_SIZE, BLOCK_SIZE), position=(BLOCK_SIZE*column, BLOCK_SIZE*line))
                     # print(block.hitbox.get_position(), block.hitbox.size)
-                    ObjectDrawer.draw('GREEN', display, block)
+                    ObjectView.draw('GREEN', display, block)

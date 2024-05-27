90f41c1450bfdde1a5030d3be4299b81281b4ade
TisAgore <97605431+TisAgore@users.noreply.github.com>
2024-05-27 15:59:36 +0500

    Delete comments

--- level_generator.py
+++ level_generator.py
@@ -13,9 +13,7 @@ class LevelGenerator:
         return self.__level
 
     def generate(self):
-        # level = 
         chance = 0.69
-        # chance_for_row = 1
         for line in range(3, self.__size[1]):
             for column in range(1, self.__size[0]):
                 if self.__level[line - 2][column] == 0 and self.__level[line - 3][column] == 0:
@@ -23,7 +21,6 @@ class LevelGenerator:
                         if np.random.random() > (1 - chance):
                             rand_int = np.random.random()
                             row_length = 0
-                            # while rand_int > (1 - chance_for_row + 0.15 * row_length):
                             while rand_int > 0.32 * row_length:
                                 if column+row_length < self.__size[0]:
                                     if self.__level[line - 1][column+row_length] == 0 and (self.__level[line - 1][column+row_length+1] == 0 or column+row_length+1 == self.__size[0]):
@@ -33,7 +30,3 @@ class LevelGenerator:
                                         break
                                 else:
                                     break
-
-# lvl = LevelGenerator((30, 30))
-# lvl.generate()
-# print(lvl.get_level())

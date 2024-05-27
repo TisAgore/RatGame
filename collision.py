e5856f9432acaa01b59b29d4726955778e33d17d
TisAgore <97605431+TisAgore@users.noreply.github.com>
2024-05-27 16:06:25 +0500

    Doing some collisions

--- collision.py
+++ collision.py
@@ -1,5 +1,7 @@
 import pygame as pg
 from abc import ABC, abstractmethod
+from constants import Direction
+from collections import OrderedDict
 
 class Hitbox:
     @abstractmethod
@@ -9,14 +11,6 @@ class Hitbox:
         self.__rect = pg.Rect(*position, size[0], size[1])
         self.__size = size
         self.__position = position
-
-    @abstractmethod
-    def get_corners(self):
-        self.__left_up_corner = list(self.__position)
-        self.__left_down_corner = [self.__position[0], self.__position[1] + self.__y_size]
-        self.__right_up_corner = [self.__position[0] + self.__x_size, self.__position[1]]
-        self.__right_down_corner = [self.__position[0] + self.__x_size, self.__position[1] + self.__y_size]
-        return self.__left_up_corner, self.__left_down_corner, self.__right_up_corner, self.__right_down_corner
     
     @property
     def size(self):
@@ -53,16 +47,12 @@ class StaticHitbox(Hitbox):
     @property
     def hitbox_type(self):
         return self.__hitbox_type
-    
-    def get_corners(self):
-        return super().get_corners()
 
 class DynamicHitbox(Hitbox):
     def __init__(self, size: tuple[int, int], position: tuple[int, int], direction: int):
         super().__init__(size, position)
         self.__collision_type = RatAndBlockCollision()
         self.direction = direction     # 1 - right, -1 - left
-        # self.__parent = parent
         CollisionDetector.add_dynamic_object(self)
 
     @property
@@ -78,14 +68,10 @@ class DynamicHitbox(Hitbox):
     
     def set_position(self, new_pos: tuple[int, int]):
         super().set_position(new_pos)
-        # self.rect = pg.Rect(*new_pos, self.size[0], self.size[1])
     
     @property
     def collision_type(self):
         return self.__collision_type
-    
-    def get_corners(self):
-        return super().get_corners()
 
     def change_x_position(self, move_to: int):
         super().set_position((self.get_position()[0] + move_to, self.get_position()[1]))
@@ -115,13 +101,35 @@ class CollisionDetector:
         moving_hitbox = None
         for object in cls.dynamic_objects_on_display:
             moving_hitbox = object
-            corners = moving_hitbox.get_corners()
             collision = moving_hitbox.rect.collidelist(cls.static_objects_rects)
             if collision != -1:
-                object_corners = cls.static_objects_on_display[collision].get_corners()
-                moving_hitbox.collision_type.action(moving_hitbox, corners[1][1], object_corners[2][1])
+                unmoveable_hitbox = cls.static_objects_on_display[collision]
+                deltas = [unmoveable_hitbox.rect.bottom - moving_hitbox.rect.top,
+                          moving_hitbox.rect.bottom - unmoveable_hitbox.rect.top,
+                          moving_hitbox.rect.right - unmoveable_hitbox.rect.left,
+                          unmoveable_hitbox.rect.right - moving_hitbox.rect.left]
+                directions_for_deltas = [Direction.TOP_DIRECTION, Direction.BOTTOM_DIRECTIOM, Direction.LEFT_DIRECTION, Direction.RIGHT_DIRECTION]
+                deltas_dict = OrderedDict()
+                for delta_id in range(len(deltas)):
+                    delta = deltas[delta_id]
+                    direction = directions_for_deltas[delta_id]
+                    if delta in deltas_dict:
+                        deltas_dict[delta] += [direction]
+                    else:
+                        deltas_dict[delta] = [direction]
+                max_delta = max(deltas)
+                collision_directions = deltas_dict[max_delta]
+                if len(collision_directions) == 2:
+                    moving_hitbox.collision_type.action(moving_hitbox, unmoveable_hitbox, max_delta, y_direction=collision_directions[0], x_direction=collision_directions[1])
+                else:
+                    collision_direction = collision_directions[0]
+                    if abs(collision_direction) == 1:
+                        moving_hitbox.collision_type.action(moving_hitbox, unmoveable_hitbox, max_delta, x_direction=collision_direction)
+                    else:
+                        moving_hitbox.collision_type.action(moving_hitbox, unmoveable_hitbox, max_delta, y_direction=collision_direction)
             else:
-                moving_hitbox.collision_type.set_status_for_player(False)
+                moving_hitbox.collision_type.above_collision_status = False
+                moving_hitbox.collision_type.below_collision_status = False
 
 
 class Collision(ABC):
@@ -131,17 +139,20 @@ class Collision(ABC):
 
 class RatAndBlockCollision(Collision):
     def __init__(self):
-        self.__collision_status = False
+        self.above_collision_status = False
+        self.below_collision_status = False
 
-    def action(self, player_hitbox: DynamicHitbox, player_hitbox_y: int, block_hitbox_y: int):
-        self.set_status_for_player(True)
-        if block_hitbox_y != (player_hitbox_y - 1):
-            moving = block_hitbox_y - player_hitbox_y + 1
-            player_hitbox.change_y_position(move_to=moving)
-
-    
-    def set_status_for_player(self, status: bool):
-        self.__collision_status = status
+    def action(self, player_hitbox: DynamicHitbox, block_hitbox: StaticHitbox, unification: int, y_direction: int = 0, x_direction: int = 0):
+        if y_direction != 0:
+            y_direction = int(y_direction/abs(y_direction))
+            if y_direction == -1:
+                self.above_collision_status = True
+            else:
+                self.below_collision_status = True
+        moving_y = ((player_hitbox.size[1] + block_hitbox.size[1]) - unification - 1) * y_direction
+        moving_x = ((player_hitbox.size[0] + block_hitbox.size[0]) - unification) * -x_direction
+        player_hitbox.change_y_position(move_to=moving_y)
+        player_hitbox.change_x_position(move_to=moving_x)
 
     def get_status_for_player(self):
         return self.__collision_status
